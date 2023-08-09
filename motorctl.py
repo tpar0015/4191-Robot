a6582d81b898c5e34f8468aeed98101a09b0b49e
HanlinXu <hxuu0046@student.monash.edu>
2023-08-09 13:10:56 +1000

    Motor test 1
    Testing the functionality of motor using R pi 4

--- motorctl.py
+++ motorctl.py
@@ -3,22 +3,36 @@ from gpiozero import PWMLED
 from time import sleep
 
 
-
-Dir = Enum('Dir, ['FORWARD', 'REVERSE']')
+Dir = Enum("Dir", ["FORWARD", "REVERSE"])
 
 # Set parameters
-pwm_freq = 18000 # Hz (20kHz max)
+pwm_freq = 18000  # Hz (20kHz max)
 direction = Dir.FORWARD
-speed = 100 # 0 to 100 (%)
+speed = 100  # 0 to 100 (%)
 
 
 # Init pins and PWM
+# NOTE:
+# .value is the PWM 0-1 which control the duty cycle(speed)
 
-OUT_A = PWMLED(12, frequency=pwm_freq)
-OUT_B = PWMLED(13, frequency=pwm_freq)
+# left wheel
+Motor_L_F = PWMLED(12, frequency=pwm_freq)  # FORWARD
+Motor_L_B = PWMLED(18, frequency=pwm_freq)  # BACKWARD
 
-OUT_A.value = speed
-OUT_B.value = speed
+Motor_L_F.off()
+Motor_L_B.off()
+# right wheel
+Motor_R_F = PWMLED(13, frequency=pwm_freq)  # FORWARD
+Motor_R_B = PWMLED(19, frequency=pwm_freq)  # BACKWARD
 
+Motor_R_F.off()
+Motor_R_B.off()
 # TODO: Logic to set pins based on direction
-#? docs: https://www.pololu.com/product/2997
+# ? docs: https://www.pololu.com/product/2997
+
+
+# moving forward
+Motor_L_F.on()
+Motor_L_F.value = 0.5
+Motor_R_F.on()
+Motor_R_F.value = 0.5
