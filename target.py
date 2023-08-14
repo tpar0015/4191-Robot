a538c402a1cac433839ed40ad75c977182cc85f0
Samin Haque <shaq0003@student.monash.edu>
2023-08-15 00:53:16 +1000

    Initialise camera

--- target.py
+++ target.py
@@ -1,10 +1,20 @@
 from time import sleep
 from picamera import PiCamera
+import cv2
 
+camera = PiCamera()
+camera.resolution = (2592, 1944) # check version of NoIR camera
+camera.start_preview()
+# Camera warm-up time
+sleep(2)
+
+# Capture a photo
+camera.capture('foo.jpg')
+
+x_pos, y_pos, x_target, y_target = None, None, None, None
 
 def localise():
-    x_pos, y_pos, x_target, y_target = None, None, None, None
-
+    
     # check camera
     # find corners of arena
     return x_pos, y_pos, x_target, y_target
