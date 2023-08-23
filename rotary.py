72c9d585b8ccb23a16c5a5c523b5b3347c4b3e57
sandflavoured <saminhaque.sh@gmail.com>
2023-08-23 11:00:18 +1000

    Updated rotary encoder code

--- rotary.py
+++ rotary.py
@@ -1,26 +1,27 @@
 # Provides functions to stop motors depending on rotary encoder output
-import rotaryio
+from gpiozero import RotaryEncoder
+
 
 pin_rotary_a = 1    #? change later
 pin_rotary_b = 2
 
-enc = rotaryio.IncrementalEncoder(pin_a=pin_rotary_a, pin_b=pin_rotary_b, divisor=1)
+rotor = RotaryEncoder(pin_rotary_a, pin_rotary_b, max_steps=0)
 
 last_position = None
 def printPosition():
-    position = enc.position
+    position = rotor.steps
     if last_position == None or position != last_position:
         print(position)
     last_position = position
 
-#! review upon testing
-def stopAfterTicks(ticks, motor1="", motor2=""):
-    initialPosition = enc.position
-    diff = 0
-    while abs(diff) < ticks:
-        newPosition = enc.position
-        diff = newPosition - initialPosition
-    if motor1:
-        motor1.off()
-    if motor2:
-        motor2.off()
+# #! review upon testing
+# def stopAfterTicks(ticks, motor1="", motor2=""):
+#     initialPosition = enc.position
+#     diff = 0
+#     while abs(diff) < ticks:
+#         newPosition = enc.position
+#         diff = newPosition - initialPosition
+#     if motor1:
+#         motor1.off()
+#     if motor2:
+#         motor2.off()
