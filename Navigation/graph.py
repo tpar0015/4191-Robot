0163a9793a0b6433c359ba29618a7f968c9901d4
HanlinXu <hxuu0046@student.monash.edu>
2023-09-05 22:55:52 +1000

    Update graph.py
    ts

--- Navigation/graph.py
+++ Navigation/graph.py
@@ -11,31 +11,26 @@ class Node:
     def __init__(self, name):
         self.name = name
         self.visited = False
-        self.distance = float('inf')
+        self.distance = float("inf")
         self.neighbours = []
         self.prev_node = None
-        self.xy = [float('inf'), float('inf')]
+        self.xy = [float("inf"), float("inf")]
         self.is_obstacle = False
-
 
     def add_neighbour(self, neighbour, weight):
         self.neighbours.append((neighbour, weight))
 
-
     def __lt__(self, other_node):
         return self.distance < other_node.distance
-
-
-
 
 
 class Graph:
     """
     Contains graph module, with shortest path methods.
     """
+
     def __init__(self):
         self.nodes = {}
-
 
     def add_node(self, node: Node):
         self.nodes[node.name] = node
@@ -43,22 +38,22 @@ class Graph:
     def __getitem__(self, pos) -> Node:
         """get_node: Returns node given name of node"""
         i, j = pos
-        return self.nodes[f'({i},{j})']
+        return self.nodes[f"({i},{j})"]
 
     def distance(self, pos1, pos2) -> float:
         """distance: Helper function, returns distance between two positions."""
         x1, y1 = pos1
         x2, y2 = pos2
-        return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+        return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
     def get_nearest_node(self, pos):
         """get_nearest_node: Takes in a (x,y) position, returns nearest node on map"""
-        min_dist = float('inf')
+        min_dist = float("inf")
         min_node = None
         x1, y1 = pos
         for node_name in self.nodes:
             node = self.nodes[node_name]
-            dist = self.distance((x1,y1), node.xy)
+            dist = self.distance((x1, y1), node.xy)
             if dist < min_dist and node.is_obstacle == False:
                 min_dist = dist
                 min_node = node
@@ -69,7 +64,7 @@ class Graph:
         """adjacent_nodes:  returns surrounding nodes within a radius"""
 
         def recursive_nodes(node, radius, pos, memo) -> None:
-            for neighbour,_ in node.neighbours:
+            for neighbour, _ in node.neighbours:
                 memo.append(node)
                 if self.distance(pos, neighbour.xy) < radius and neighbour not in memo:
                     recursive_nodes(neighbour, radius, pos, memo)
@@ -85,16 +80,13 @@ class Graph:
         node.is_obstacle = True
         for neighbour, _ in node.neighbours:
             neighbour.neighbours = [x for x in neighbour.neighbours if x[0] != node]
+            # ?
         node.neighbours = []
-
-    
-
 
     def djikstras(self, start_node, target_node) -> None:
         """djikstras: Returns shortest path between two nodes"""
         start_node.distance = 0
         heap = [(0, start_node)]
-
 
         while heap:
             current_distance, current_node = heapq.heappop(heap)
@@ -107,7 +99,7 @@ class Graph:
 
             current_node.visited = True
 
-            for neighbour,  weight in current_node.neighbours:
+            for neighbour, weight in current_node.neighbours:
                 if not neighbour.visited:
                     new_distance = current_distance + weight
                     if new_distance < neighbour.distance:
@@ -115,38 +107,34 @@ class Graph:
                         neighbour.prev_node = current_node
                         heapq.heappush(heap, (new_distance, neighbour))
 
-
-
-    def get_shortest_distance(self, target: Node) :
+    def get_shortest_distance(self, target: Node):
         """get_shortest_distance: Returns shortest distance and path to target node"""
         path = []
         current_node = target
 
         while current_node.prev_node is not None:
-            path.insert(0,current_node.name)
+            path.insert(0, current_node.name)
             current_node = current_node.prev_node
-        path.insert(0,current_node.name)
+        path.insert(0, current_node.name)
         return target.distance, path
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     nodes = [[Node(f"({row},{col})") for col in range(3)] for row in range(3)]
 
     for row in range(3):
         for col in range(3):
             if row > 0:
-                nodes[row][col].add_neighbour(nodes[row - 1][col], 1) # Up
+                nodes[row][col].add_neighbour(nodes[row - 1][col], 1)  # Up
 
             if row < 2:
-                nodes[row][col].add_neighbour(nodes[row + 1][col], 1) # Down
+                nodes[row][col].add_neighbour(nodes[row + 1][col], 1)  # Down
 
             if col > 0:
-                nodes[row][col].add_neighbour(nodes[row][col - 1], 1) # Left
+                nodes[row][col].add_neighbour(nodes[row][col - 1], 1)  # Left
 
             if col < 2:
-                nodes[row][col].add_neighbour(nodes[row][col + 1], 1) # Right
-
+                nodes[row][col].add_neighbour(nodes[row][col + 1], 1)  # Right
 
     G = Graph()
     for row in nodes:
@@ -155,7 +143,7 @@ if __name__ == '__main__':
 
     start_node = nodes[1][1]
     target_node = nodes[2][2]
-    #G.djikstras(start_node, target_node)
-    #path_test, dist = G.get_shortest_distance(target_node)
-    #print(path_test, dist)
+    # G.djikstras(start_node, target_node)
+    # path_test, dist = G.get_shortest_distance(target_node)
+    # print(path_test, dist)
     print(G[0, 0])
